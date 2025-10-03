72a6db50af058f76bad88b5c9da46a1ac153a960
Okamoto Yayoi <oyayoi@c2r3s10.42tokyo.jp>
2025-10-03 23:29:54 +0900

    done_echo

--- src/builtin/builtin.c
+++ src/builtin/builtin.c
@@ -3,12 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   builtin.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: okamotoyayoi <okamotoyayoi@student.42.f    +#+  +:+       +#+        */
+/*   By: oyayoi <oyayoi@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/02 21:54:51 by okamotoyayo       #+#    #+#             */
-/*   Updated: 2025/10/03 14:02:20 by okamotoyayo      ###   ########.fr       */
+/*   Updated: 2025/10/03 23:29:21 by oyayoi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+
+int	builtin_echo(char **argv);
 
 void	ft_putstr_fd(char *s, int fd)
 {
@@ -26,7 +33,7 @@ void	ft_putstr_fd(char *s, int fd)
 
 int	ft_strcmp(const char *s1, const char *s2)
 {
-	while (*s1 && *s2 && (*s1 == *s2))
+	while (*s1 && (*s1 == *s2))
 	{
 		s1++;
 		s2++;
@@ -58,32 +65,70 @@ int	exec_builtin(char **argv)
 {
 	if (ft_strcmp(argv[0], "echo") == 0)
 		return (builtin_echo(argv));
-	else if (ft_strcmp(argv[0], "cd") == 0)
-		return (builtin_cd(argv));
-	else if (ft_strcmp(argv[0], "pwd") == 0)
-		return (builtin_pwd());
-	else if (ft_strcmp(argv[0], "export") == 0)
-		return (builtin_export(argv));
-	else if (ft_strcmp(argv[0], "unset") == 0)
-		return (builtin_unset(argv));
-	else if (ft_strcmp(argv[0], "env") == 0)
-		return (builtin_env());
-	else if (ft_strcmp(argv[0], "exit") == 0)
-		return (builtin_exit(argv));
+	// else if (ft_strcmp(argv[0], "cd") == 0)
+	// 	return (builtin_cd(argv));
+	// else if (ft_strcmp(argv[0], "pwd") == 0)
+	// 	return (builtin_pwd());
+	// else if (ft_strcmp(argv[0], "export") == 0)
+	// 	return (builtin_export(argv));
+	// else if (ft_strcmp(argv[0], "unset") == 0)
+	// 	return (builtin_unset(argv));
+	// else if (ft_strcmp(argv[0], "env") == 0)
+	// 	return (builtin_env());
+	// else if (ft_strcmp(argv[0], "exit") == 0)
+	// 	return (builtin_exit(argv));
 	else
 		return (0);
 }
 
 int	check_n_option(char *opt)
 {
-	int i;
+	int	i;
+
 	i = 1;
-	if (!opt)
+	if (!opt || opt[0] != '-')
 		return (0);
-	
+	while (opt[i])
+	{
+		if (opt[i] != 'n')
+			return (0);
+		i++;
+	}
+	if (i == 1)
+		return (0);
+	return (1);
 }
 
 int	builtin_echo(char **argv)
 {
-	while (*argv && )
+	int	newline;
+	int	is_first;
+	int	i;
+
+	newline = 1;
+	is_first = 1;
+	i = 1;
+	while (argv[i] && check_n_option(argv[i]))
+	{
+		newline = 0;
+		i++;
+	}
+	while (argv[i])
+	{
+		if (!is_first)
+			ft_putstr_fd(" ", STDOUT_FILENO);
+		ft_putstr_fd(argv[i], STDOUT_FILENO);
+		is_first = 0;
+		i++;
+	}
+	if (newline)
+		ft_putstr_fd("\n", STDOUT_FILENO);
+	return (0);
+}
+
+int main(int argc, char **argv)
+{
+	if (argc > 1)
+		builtin_echo(argv);
+	return (0);
 }
